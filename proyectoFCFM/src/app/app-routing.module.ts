d625011e7a8d5568da8e64eed54870e775159c61
issacramirez <sowen818@gmail.com>
2020-05-09 10:51:09 -0500

    acomodando path

--- proyectoFCFM/src/app/app-routing.module.ts
+++ proyectoFCFM/src/app/app-routing.module.ts
@@ -2,15 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
 ];
 
 @NgModule({
