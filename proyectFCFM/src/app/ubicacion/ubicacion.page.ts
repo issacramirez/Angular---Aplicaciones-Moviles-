0614627ea48dc2529b5e13186ecb6bc2b098e0af
JuanD254 <juan.daniel.moron@gmail.com>
2020-05-31 23:51:04 -0500

    Estoy agregando lo de geolocalizacion y cree una nueva page llamada ubicacion

---
+++ proyectFCFM/src/app/ubicacion/ubicacion.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  ToastController,
+  Platform
+} from '@ionic/angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  Marker,
+  GoogleMapsAnimation,
+  MyLocation
+} from '@ionic-native/google-maps';
+
+@Component({
+  selector: 'app-ubicacion',
+  templateUrl: './ubicacion.page.html',
+  styleUrls: ['./ubicacion.page.scss'],
+})
+export class UbicacionPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
