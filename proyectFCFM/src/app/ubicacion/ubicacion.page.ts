fff333a0c6db47d4f4a3170ce6332d235f3af09e
issacramirez <sowen818@gmail.com>
2020-06-12 03:43:32 -0500

    quitando codigo inservible

--- proyectFCFM/src/app/ubicacion/ubicacion.page.ts
+++ proyectFCFM/src/app/ubicacion/ubicacion.page.ts
@@ -3,13 +3,6 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 declare var google;
 
-interface Marker {
-  position: {
-    lat: number,
-    lng: number,
-  };
-  title: string;
-}
 
 @Component({
   selector: 'app-ubicacion',
@@ -42,13 +35,9 @@ export class UbicacionPage implements OnInit {
 
 
   loadMap() {
-    // create a new map by passing HTMLElement
     const mapEle: HTMLElement = document.getElementById('map');
-    // acceder a los indicadores
     const indicatorsEle: HTMLElement = document.getElementById('indicators');
-    // create LatLng object
     const myLatLng = {lat: 25.725542, lng: -100.315187};
-    // create map
     this.map = new google.maps.Map(mapEle, {
       center: myLatLng,
       zoom: 13.2
